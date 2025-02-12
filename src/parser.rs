048ad0cce836841081aecc4ae4b1fded530cbea8
darkartx <darkartx@gmail.com>
2025-02-12 15:41:09 +0400

    *

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,17 @@
+/*
+https://craftinginterpreters.com/parsing-expressions.html
+
+expression     → equality ;
+equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+term           → factor ( ( "-" | "+" ) factor )* ;
+factor         → unary ( ( "/" | "*" ) unary )* ;
+unary          → ( "!" | "-" ) unary
+               | primary ;
+primary        → NUMBER | STRING | "true" | "false" | "nil"
+               | "(" expression ")" ;
+*/
+
 use std::iter;
 
 use super::{
